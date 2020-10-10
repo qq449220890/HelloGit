2d074d3533855052bb1b0966b19ebd541388d721
18818405902 <liu_wen_yong_work@163.com>
2020-10-10 17:11:51 +0800

    创建分支2

--- HelloGit/src/main/java/com/lwy/git/HelloGit.java
+++ HelloGit/src/main/java/com/lwy/git/HelloGit.java
@@ -4,5 +4,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("创建分支");
+        System.out.println("主干添加");
+        System.out.println("创建分支2");
     }
 }
